c222e6a2a61e3bc205ec17fd0a3dd75b0a73add3
Stephanie Claudino Daffara <stephaniecd@berkeley.edu>
2018-10-18 20:07:06 -0700

    able to store meanface

--- meanFace.py
+++ meanFace.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 from os import listdir
 from utils import point as p
+from utils import Profiler
 import numpy as np
+import morph
+from morph import makeInterp
+from utils import show
+import scipy
 
 
 def getPoints(path):
@@ -13,17 +18,89 @@ def getPoints(path):
 
     return np.array(pts)
 
+def warpBaryMulti(imgs, vtxs, interpk, warpk):
+    numOfImgs = imgs.shape[0]
+    interps = [makeInterp(imgs[i], interpk) for i in range(numOfImgs)]
+    M = np.zeros_like(imgs[0])
+
+    vtxM = np.mean(vtxs, axis=0)
+    delunay = scipy.spatial.Delaunay(vtxM, qhull_options='QJ')
+
+    x = np.repeat(np.arange(M.shape[1]), M.shape[0])
+    y = np.tile(np.arange(M.shape[0]), M.shape[1])
+
+    triIdx = delunay.simplices
+    numOfTris = triIdx.shape[0]
+    triM = np.vstack((delunay.points[triIdx] for _ in range(numOfImgs)))
+    T = np.vstack((vtxs[i, triIdx] for i in range(numOfImgs)))
+    T = np.swapaxes(T, 1, 2)
+
+    vtxM = np.array([x, y]).T
+    triIdx = delunay.find_simplex(vtxM)
+    vtxM = np.vstack([vtxM] * numOfImgs)
+    triIdx = np.hstack([triIdx + numOfTris * i for i in range(numOfImgs)])
+
+    pm_b = morph.baryentricMulti(vtxM, triM[triIdx, 0], triM[triIdx, 1], triM[triIdx, 2])[..., np.newaxis]
+    pi = T[triIdx] @ pm_b
+    n = x.size
+
+    baryImgs = np.array([
+        interps[i](pi[n * i: n * i + n][:, 0], pi[n * i: n * i + n][:, 1], grid=False)
+        for i in range(numOfImgs)
+    ])
+
+    M[y, x] = np.mean(baryImgs, axis=0)
+    return M, delunay.points.tolist()
+
+def daisyChainMulti(images, points):
+    midsvtx, midstri = morph.getMid(points)
+    A = images[0]
+    vtxA = points[0]
+    for i in range(1, len(images)):
+        B, vtxB = images[i, :, :], points[i, :, :]
+
+        warpK = (1 / (i + 1))
+        dissolveK = warpK
+        # morph.showTriangulation(A, B, vtxA, vtxB, warpK, (midsvtx, midstri))
+        # plt.imshow(A, cmap='Greys_r')
+
+        A = morph.morph(A, B, vtxA, vtxB, warpK, dissolveK, (midsvtx, midstri))
+        vtxA = warpK * vtxA + (1 - warpK) * vtxB
+
+        # if i%5==0:
+        #     print('{}% completed'.format((i/len(images))*100))
+        # morph.showTriangulation(A, B, vtxA, vtxB, warpK, (midsvtx, midstri))
+
+    plt.imshow(A, cmap='Greys_r')
+    plt.show()
+
 def init():
 
-    pathFaces = 'in/brazil_faces/'
-    pathPoints = 'in/brazil_faces_pts/'
-    images = np.array([])
-    points = np.array([])
+    with Profiler():
+        pathFaces = 'in/brazil_subset/'
+        pathPoints = 'in/brazil_faces_pts/'
+        outPath = 'out/brazil_faces/'
+        fileName = 'meanFace'
+        images = []
+        points = []
+        for file in listdir(pathFaces):
+            if 'a' in file:
+                img = p.normalize(plt.imread(pathFaces + file))
+                pts = getPoints(pathPoints + file.split('.')[0] + '.pts')
+                corners = [(0, 0), (0, img.shape[0] - 1), (img.shape[1] - 1, 0), (img.shape[1] - 1, img.shape[0] - 1)]
+                pts = np.append(corners, pts, axis=0)
+                points.append(pts)
+                images.append(img)
 
-    for file in listdir(pathFaces):
+        images = np.stack(images)
+        points = np.stack(points)
 
-        if 'a' in file:
-            img = p.normalize(plt.imread(pathFaces + file))
-            pts = getPoints(pathPoints + file.split('.')[0] + '.pts')
+        # daisyChainMulti(images, points)
+        A, vtxA = warpBaryMulti(images, points, 1, 1)
 
+        morph.saveJSON(vtxA, outPath, fileName)
+        morph.storeImg(A, outPath, fileName)
+
+        plt.imshow(A, cmap='Greys_r')
+        plt.show()
 
--- morph.py
+++ morph.py
@@ -6,6 +6,7 @@ import cv2 as cv
 import scipy.spatial
 from scipy import interpolate as interp
 from utils import ops
+from utils import Profiler
 
 
 def triangulate(vtxA, vtxB, k):
@@ -15,7 +16,7 @@ def triangulate(vtxA, vtxB, k):
 
 
 class CorrespondenceUI:
-    def __init__(self, A, B, save=True):
+    def __init__(self, A, B=[], save=True):
         self.ax, self.info, self.save = None, {}, save
         self.featuresOk = False
         self.fig = plt.figure(figsize=(10, 5))
@@ -109,6 +110,10 @@ class CorrespondenceUI:
         with open(self.save, 'wt') as file:
             json.dump(info, file, indent=2)
 
+def saveJSON(info, folder, fileName):
+    if not os.path.exists(os.path.dirname(folder)): os.mkdir(folder)
+    with open(folder+fileName, 'wt') as file:
+        json.dump(info, file, indent=2)
 
 def computeAffine(tri1, tri2):
     xcoord1, ycoord1 = tri1[1] - tri1[0], tri1[2] - tri1[0]
@@ -118,14 +123,43 @@ def computeAffine(tri1, tri2):
     return T2 @ T1
 
 
-def morph(A, B, vtxA, vtxB, warpK, dissolveK):
-    M = np.zeros(A.shape)
+def makeInterp(X, k=1):
+    X = X[..., np.newaxis] if len(X.shape) == 2 else X
+    idx = np.arange(X.shape[-1])
+    x, y = np.arange(X.shape[1]), np.arange(X.shape[0])
 
-    midVtxs = warpK * vtxA + (1 - warpK) * vtxB
+    interps = [
+        scipy.interpolate.RectBivariateSpline(x, y, X[..., i].T, kx=k, ky=k)
+        for i in idx
+    ]
+
+    def interpFunc(x, y, **kwargs):
+        val = [i(x, y, **kwargs) for i in interps]
+        val = np.array(val).T.squeeze()
+        return val
+
+    return interpFunc
+
+
+def getMid(vtxs):
+    midVtxs = np.sum(vtxs, axis=0) / vtxs.shape[0]
     midTris = scipy.spatial.Delaunay(midVtxs, qhull_options='QJ')
 
-    interpA = interp.RectBivariateSpline(np.arange(A.shape[1]), np.arange(A.shape[0]), A.T)
-    interpB = interp.RectBivariateSpline(np.arange(B.shape[1]), np.arange(B.shape[0]), B.T)
+    return midVtxs, midTris
+
+
+def morph(A, B, vtxA, vtxB, warpK, dissolveK, mid=None ):
+    M = np.zeros(A.shape)
+
+    if not mid:
+        midVtxs = warpK * vtxA + (1 - warpK) * vtxB
+        midTris = scipy.spatial.Delaunay(midVtxs, qhull_options='QJ')
+    else:
+        midVtxs = mid[0]
+        midTris = mid[1]
+
+    interpA = makeInterp(A)
+    interpB = makeInterp(B)
 
     x = np.repeat(np.arange(M.shape[1]), M.shape[0])
     y = np.tile(np.arange(M.shape[0]), M.shape[1])
@@ -150,23 +184,39 @@ def morph(A, B, vtxA, vtxB, warpK, dissolveK):
     return M
 
 
-def morphSequence(A, B, vtxA, vtxB, framesK):
+def storeImgs(images, folder):
+    path = 'out/{}/'.format(folder)
+    if not os.path.exists(os.path.dirname(path)): os.mkdir(path)
+    [cv.imwrite(path+'{}.jpg'.format(i), (im * 255).astype(np.uint8)) for i, im in enumerate(images, 0)]
+
+def storeImg(image, folder, fileName):
+    if not os.path.exists(os.path.dirname(folder)): os.mkdir(path)
+    img = (image * 255).astype(np.uint8)
+    cv.imwrite(folder+fileName+'{}.jpg'.format(fileName), img)
+
+
+def morphSequence(A, B, vtxA, vtxB, framesK, out, makeVideo=False, isColor=True):
     images = []
     for i in range(1, framesK + 1):
         frac = i / framesK
-        BGR = [
-            morph(A[:, :, 0], B[:, :, 0], vtxA, vtxB, frac, frac).clip(0, 1),
-            morph(A[:, :, 1], B[:, :, 1], vtxA, vtxB, frac, frac).clip(0, 1),
-            morph(A[:, :, 2], B[:, :, 2], vtxA, vtxB, frac, frac).clip(0, 1)
-        ]
-        img = np.dstack(reversed(BGR))
+        img = morph(A, B, vtxA, vtxB, frac, frac).clip(0,1)
+        if isColor : img = img[...,::-1]
         images.append(img)
 
-    imgs2video(images)
+        print('{}% completed'.format((i/framesK)*100))
+
+    reverse = images.copy()
+    reverse.reverse()
+    images.extend(reverse)
+
+    if makeVideo:
+        storeImgs(images, out)
+        imgs2video(images, out)
+    else: storeImgs(images)
 
 
-def imgs2video(images):
-    videoPath = 'out/seq0.mp4'
+def imgs2video(images, out):
+    videoPath = 'out/{}.mp4'.format(out)
     fps = 30
     frame = images[0]
     height, width, channels = frame.shape
@@ -199,47 +249,124 @@ def baryentric(p, a, b, c):
     return u[..., np.newaxis], v[..., np.newaxis], w[..., np.newaxis]
 
 
-def doColorMorph(A, B, a, b):
-    warpK = 0.5
-    dissolveK = 0.5
+def baryentricMulti(p, a, b, c):
+    v0, v1, v2 = b - a, c - a, p - a
+    d00 = np.einsum('ij, ij->i', v0, v0)
+    d01 = np.einsum('ij, ij->i', v0, v1)
+    d11 = np.einsum('ij, ij->i', v1, v1)
+    d20 = np.einsum('ij, ij->i', v2, v0)
+    d21 = np.einsum('ij, ij->i', v2, v1)
 
-    RGB = [
-        morph(A[:, :, 0], B[:, :, 0], a, b, warpK, dissolveK).clip(0, 1),
-        morph(A[:, :, 1], B[:, :, 1], a, b, warpK, dissolveK).clip(0, 1),
-        morph(A[:, :, 2], B[:, :, 2], a, b, warpK, dissolveK).clip(0, 1)
-    ]
-
-    return np.dstack(RGB)
+    denom = 1. / (d00 * d11 - d01 * d01)
+    v = (d11 * d20 - d01 * d21) * denom
+    w = (d00 * d21 - d01 * d20) * denom
+    u = 1. - v - w
+    return np.array([u,v,w]).T
 
 
-def showTriangulation(A, B, a, b):
+def doColorMorph(A, B, a, b, warpK, dissolveK):
+
+    img = morph(A, B, a, b, warpK, dissolveK).clip(0, 1)[..., ::-1]
+
+    return img
+
+
+def showTriangulation(A, B, a, b, warpK, mid=None):
+
+    if not mid:
+        midVtxs = warpK * a + (1 - warpK) * b
+        midTris = scipy.spatial.Delaunay(midVtxs, qhull_options='QJ')
+    else:
+        midVtxs = mid[0]
+        midTris = mid[1]
 
     fig = plt.figure(figsize=(10, 5))
     fig.subplots_adjust(hspace=0.1, wspace=0.1)
 
     ax1 = fig.add_subplot(1, 2, 1)
-    ax1.imshow(A, interpolation=None)
+    ax1.imshow(A, interpolation=None, cmap='Greys_r')
     ax1.triplot(a[:, 0], a[:, 1], midTris.simplices)
     ax1.plot(a[:, 0], a[:, 1], '+')
 
     ax2 = fig.add_subplot(1, 2, 2)
-    ax2.imshow(B, interpolation=None)
+    ax2.imshow(B, interpolation=None, cmap='Greys_r')
     ax2.triplot(b[:, 0], b[:, 1], midTris.simplices)
     ax2.plot(b[:, 0], b[:, 1], '+')
 
     plt.show()
 
 
+def warpInv2(A, vtxA, B, vtxB, k, t):
+    interpA = makeInterp(A, k=k)
+    interpB = makeInterp(B, k=k)
+
+    M = np.zeros_like(A)
+    d = triangulate(vtxA, vtxB, t)
+
+    x = np.repeat(np.arange(M.shape[1]), M.shape[0])
+    y = np.tile(np.arange(M.shape[0]), M.shape[1])
+
+    tidx = d.simplices
+    tNum = tidx.shape[0]
+    triM = d.points[tidx]
+    triA = vtxA[tidx]
+    triB = vtxB[tidx]
+
+    # Compute one transform for each triangle:
+    T = np.zeros((tNum, 3, 3, 2))
+
+    basis = np.array([triM[:, 1] - triM[:, 0], triM[:, 2] - triM[:, 0]])
+    basis = np.moveaxis(basis, 0, -1)
+    T[:, :-1, :-1, 0] = basis
+    T[:, :-1, -1, 0] = triM[:, 0]
+    T[:, -1, -1, 0] = 1
+    T_ = np.linalg.pinv(T[..., 0])
+    T[:] = T_[..., np.newaxis]
+
+    Ts2a = np.zeros((tNum, 3, 3))
+    basis = np.array([triA[:, 1] - triA[:, 0], triA[:, 2] - triA[:, 0]])
+    basis = np.moveaxis(basis, 0, -1)
+    Ts2a[:, :-1, :-1] = basis
+    Ts2a[:, :-1, -1] = triA[:, 0]
+    Ts2a[:, -1, -1] = 1
+
+    Ts2b = np.zeros((tNum, 3, 3))
+    basis = np.array([triB[:, 1] - triB[:, 0], triB[:, 2] - triB[:, 0]])
+    basis = np.moveaxis(basis, 0, -1)
+    Ts2b[:, :-1, :-1] = basis
+    Ts2b[:, :-1, -1] = triB[:, 0]
+    Ts2b[:, -1, -1] = 1
+
+    T[..., 0] = Ts2a @ T[..., 0]
+    T[..., 1] = Ts2b @ T[..., 1]
+
+    # Process each point:
+    # tidx indexes into the right transform
+    # (i.e. for the triangle the point belongs to)
+    pm = np.array([x, y]).T
+    tidx = d.find_simplex(pm)
+
+    pm_ = np.column_stack((pm, np.ones(pm.shape[0])))[..., np.newaxis]
+    pa = (T[tidx, ..., 0] @ pm_)
+    pb = (T[tidx, ..., 1] @ pm_)
+
+    aVal = interpA(pa[:, 0], pa[:, 1], grid=False)
+    bVal = interpB(pb[:, 0], pb[:, 1], grid=False)
+    M[y, x] = t * aVal + (1-t) * bVal
+    return M
+
+
 def init():
     capture = False
 
-    path1 = 'in/georgeSmall.jpeg'
-    path2 = 'in/boySmall.jpeg'
+    path1 = 'in/ste-child.jpg'
+    path2 = 'in/child-ste.jpg'
     A = ops.point.normalize(plt.imread(path1))
     B = ops.point.normalize(plt.imread(path2))
-
+    warpK = 0.5
+    dissolveK = 0.5
     img1, img2 = os.path.basename(path1).split('.')[0], os.path.basename(path2).split('.')[0]
-    coordsPath = 'out/{}-{}-v2.json'.format(img1, img2)
+    coordsPath = 'out/{}-{}v2.json'.format(img1, img2)
 
     if capture:
         coords = CorrespondenceUI(A, B, coordsPath).capture()
@@ -249,9 +376,17 @@ def init():
             coords = json.load(file)
 
     a, b = np.array(coords['A']), np.array(coords['B'])
-    video = morphSequence(A, B, a, b, 40)
+    outPath = '{}-{}'.format(img1, img2)
 
-    # img = doColorMorph(A, B, a, b)
-    # showTriangulation()
+    with Profiler():
+        morphSequence(A, B, a, b, 2, outPath, makeVideo=True)
+
+    # showTriangulation(A, B, a, b, 0.5)
+    # img = doColorMorph(A, B, a, b, warpK, dissolveK)
+    # img = warpInv2(A, a, B, b, 1, 0.5)
+    # img = morph(A, B, a, b, warpK, dissolveK)
+
+    # plt.imshow(img)
+    # plt.show()
 
 
